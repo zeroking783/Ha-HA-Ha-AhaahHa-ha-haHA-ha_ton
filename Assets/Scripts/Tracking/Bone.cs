e76f2df8944ea4509af6d370960bb9297cea5533
Dmitry057 <dimasananas057@gmail.com>
2022-12-09 08:57:07 +0400

    # bodytracking working NO HYEVO

---
+++ Assets/Scripts/Tracking/Bone.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bone : MonoBehaviour
+{
+    public int id;
+    public Vector3 position;
+    public float visibility;
+    [SerializeField] private Transform[] drawRayTo;
+    public void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, position, 1*Time.deltaTime);
+        foreach (var item in drawRayTo)
+        {
+            Debug.DrawLine(transform.position, item.position, Color.red);
+        }
+    }
+}
---
+++ Assets/Scripts/Tracking/ZeroMQInstance.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+
+public class ZeroMQInstance: MonoBehaviour
+{
+    private Thread _thread;
+    private byte[] _data = null;
+    private bool _isRunning = false;
+    private bool pythonRunning = false;
+    
+    private Process _process;
+    
+    public ZeroMQInstance(Action<string> callback)
+    {
+        run_cmd();
+        
+        _thread =  new Thread(() =>
+        {
+            using (var socket = new RequestSocket())
+            {
+
+                while (pythonRunning)
+                {
+                    socket.Connect("tcp://localhost:5555");
+
+                    while (_isRunning)
+                    {
+
+                        if (_data != null)
+                        {
+                            socket.SendFrame(_data);
+                            _data = null;
+                            string message = socket.ReceiveFrameString();
+                            callback(message);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public void Start()
+    {
+        _isRunning = true;
+        _thread.Start();
+    }
+
+    public void OnDisable()
+    {
+        Stop();
+        print("QUIT");
+    }
+
+    public void Stop()
+    {
+        _isRunning = false;
+        _thread.Join();
+        _process.Close();
+    }
+    
+    public void Send(byte[] data)
+    {
+        _data = data;
+    }
+
+     private void run_cmd()
+        {
+
+            string fileName = @"D:\Desktop\AlgoritmsTasks\Tracking\main.py";
+
+            _process = new Process();
+            _process.StartInfo = new ProcessStartInfo(@"d:\Desktop\AlgoritmsTasks\Tracking\Scripts\python.exe", fileName)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = false
+            };
+            _process.Start();
+            
+            pythonRunning = true;
+            print("File runned");
+            
+        }
+}
